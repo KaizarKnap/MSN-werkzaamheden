57314c05490a7920aa7bff5a16da1bbd0acc1b86
Thomas Knap <t.knap@milieuservice.nl>
2025-02-20 08:35:00 +0100

    1e versie

---
+++ Pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+# laad het Excel-bestand 
+bestand= 'jouw_bestand.xlsx' # Vervang dit met de naam van je Excel-bestand
+df = pd.read_excel(bestand, engine='openpyxl')
+
+# Controleer op dubbele waarden in kolom 'AV' en voeg een nieuwe kolom 'Dubbel' toe 
+df['Dubbel'] = df['A'].duplicated(keep=false)  # keep=false om alle duplicaten te markeren
+
+# Sla het gewijzigde bestand op 
+df.to_excel('jouw_bestand_met_dubbele_waarden.xlsx',index=false, engine='openpyxl')
+
+Print("Script uitgevoerd! De dubbele waarden zijn gemarkeerd")
+
