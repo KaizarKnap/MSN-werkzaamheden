91a28e23d52dbd3dae4ca205976537a3eed02bc9
Thomas Knap <t.knap@milieuservice.nl>
2025-02-20 08:37:51 +0100

    A naar AV veranderd

--- Pandas.py
+++ Pandas.py
@@ -5,10 +5,9 @@ bestand= 'jouw_bestand.xlsx' # Vervang dit met de naam van je Excel-bestand
 df = pd.read_excel(bestand, engine='openpyxl')
 
 # Controleer op dubbele waarden in kolom 'AV' en voeg een nieuwe kolom 'Dubbel' toe 
-df['Dubbel'] = df['A'].duplicated(keep=false)  # keep=false om alle duplicaten te markeren
+df['Dubbel'] = df['AV'].duplicated(keep=false)  # keep=false om alle duplicaten te markeren
 
 # Sla het gewijzigde bestand op 
 df.to_excel('jouw_bestand_met_dubbele_waarden.xlsx',index=false, engine='openpyxl')
 
 Print("Script uitgevoerd! De dubbele waarden zijn gemarkeerd")
-
